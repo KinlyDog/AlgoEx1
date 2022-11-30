4eb5cfe41f2354092dc3e1a0f54272e188f6b61b
Andrey Otryaskin <kinlydog@gmail.com>
2022-11-30 17:13:54 +1000

    Update project

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,11 @@
 
 \
 ,.idea/inspectionProfiles/Project_Default.xml,4/9/496a238a6afa168dbaf6efd37bb459331589579c
+;
+AlgoEx1.iml,2/3/23c3eab656567e382ef72d865582837134ca5dd2
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+C
+src/LinkedList.java,0/0/00fd33ac798e066bff270304abba2030492c06ee
+=
+src/Func.java,d/9/d93a672a96ccd86152fcc314448905f52ebe2e05
--- src/LinkedListTest.java
+++ src/LinkedListTest.java
@@ -6,22 +6,69 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LinkedListTest {
+class LinkedListTestEmpty {
+    LinkedList listEmpty;
+    boolean arg = listEmpty.head == null && listEmpty.tail == null;
+
+    @BeforeEach
+    void setUp() {
+        listEmpty = new LinkedList();
+    }
+
+    @Test
+    void findAll() {
+
+    }
+
+    @Test
+    void remove() {
+        assertFalse (listEmpty.remove(5));
+    }
+
+    @Test
+    void removeAll() {
+        int n2 = listEmpty.count();
+        listEmpty.removeAll(5);
+
+        assertEquals(n2, listEmpty.count());
+    }
+
+    @Test
+    void clear() {
+        listEmpty.clear();
+
+        assertTrue(arg);
+    }
+    @Test
+    void count() {
+        assertTrue(listEmpty.count() == 0);
+    }
+
+    @Test
+    void insertAfter() {
+
+    }
+
+
+}
+
+class LinkedListTestOne {
     int listOneValue = 13;
-    int listFullValue = 10_000;
-    LinkedList listEmpty = new LinkedList();
-    LinkedList listFull = new LinkedList();
     LinkedList listOne = new LinkedList();
 
-    @BeforeEach // not ok
+    @BeforeEach
     void setUp() {
-        // one element
         listOne.addInTail(new Node(listOneValue));
+    }
 
-        // many elements
-        for (int i = 0; i < listFullValue; i++) {
-            listFull.addInTail(new Node(i));
-        }
+    @Test
+    void OneRemoveTest1() {
+        assertTrue(listOne.remove(listOneValue));
+    }
+
+    @Test
+    void OneRemoveTest2() {
+        assertFalse(listOne.remove(1));
     }
 
 /*
@@ -36,23 +83,120 @@ class LinkedListTest {
 
     }*/
 
-    @Test
-    void removeOne() {
-        assertTrue  (listOne.remove(listOneValue));
-        assertFalse (listOne.remove(1));
-        assertEquals(listOne.count(), 0);
-    }
-
-    @Test
-    void removeFull() {
-        assertTrue  (listFull.remove(2));
-        assertFalse (listFull.remove(listFullValue));
-        assertEquals(listFull.count(), listFullValue - 1);
-    }
 
     @Test
     void removeEmpty() {
-        assertFalse (listEmpty.remove(5));
+        assertFalse(listEmpty.remove(5));
+    }
+
+    @Test
+    void removeAll() {
+        // full
+        for (int i = 0; i < listFullValue; i++) {
+            listFull.removeAll(i / 2);
+        }
+
+        assertEquals(listFull.count(), listFullValue / 2);
+
+        // one
+        int n1 = listOne.count();
+        listOne.removeAll(5);
+        assertEquals(n1, listOne.count());
+
+        listOne.removeAll(listOneValue);
+        assertEquals(listOne.count(), 0);
+
+        // empty
+        int n2 = listEmpty.count();
+        listEmpty.removeAll(5);
+
+        assertEquals(n2, listEmpty.count());
+    }
+
+    @Test
+    void clear() {
+        listOne.clear();
+
+        boolean arg1 = listOne.head == null && listOne.tail == null;
+
+        assertTrue(arg1);
+    }
+
+    @Test
+    void count() {
+        assertTrue(listOne.count() == 1);
+    }
+
+    @Test
+    void insertAfter() {
+        listOne.insertAfter(listOne.tail, new Node(666));
+        assertTrue(listOne.head.value == 666);
+        assertTrue(listOne.tail.value == listOneValue);
+
+    }
+}
+
+class LinkedListTestFull {
+    int listOneValue = 13;
+    int listFullValue = 10_000;
+    LinkedList listEmpty = new LinkedList();
+    LinkedList listFull = new LinkedList();
+    LinkedList listOne = new LinkedList();
+
+    @BeforeEach
+        // not ok
+    void setUp() {
+        // one element
+        listOne.addInTail(new Node(listOneValue));
+
+        // many elements
+        for (int i = 0; i < listFullValue; i++) {
+            listFull.addInTail(new Node(i));
+        }
+    }
+
+    @Test
+    void OneRemoveTest1() {
+        assertTrue(listOne.remove(listOneValue));
+    }
+
+    @Test
+    void OneRemoveTest2() {
+        assertFalse(listOne.remove(1));
+    }
+
+    @Test
+    void FullRemoveTest1() {
+        assertTrue(listFull.remove(2));
+    }
+
+    @Test
+    void FullRemoveTest2() {
+        assertFalse(listFull.remove(listFullValue));
+    }
+
+    @Test
+    void FullRemoveTest3() {
+        assertEquals(listFull.count(), listFullValue - 1);
+    }
+
+
+/*
+    @Test
+    void findAll() {
+        ArrayList<Node> nodes = new ArrayList<>();
+        ArrayList<Node> nodesOne = new ArrayList<>();
+        nodesOne.add(new Node(0));
+
+        assertEquals(listEmpty.findAll(2), nodes);
+        assertEquals(listOne.findAll(0), nodesOne);
+
+    }*/
+
+
+    @Test
+    void removeEmpty() {
+        assertFalse(listEmpty.remove(5));
     }
 
     @Test
@@ -93,6 +237,7 @@ class LinkedListTest {
         assertTrue(arg1);
         assertTrue(arg2);
     }
+
     @Test
     void count() {
         assertTrue(listEmpty.count() == 0);
@@ -107,6 +252,6 @@ class LinkedListTest {
         assertTrue(listOne.tail.value == listOneValue);
 
     }
-
-
 }
+
+
